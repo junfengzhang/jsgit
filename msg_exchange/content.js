3ae4bdee8ff26426caceefe27d3176b0dddcf77b
zhang <zemmeleta@gmail.com>
2015-12-31 17:35:48 +0800

    11

--- msg_exchange/content.js
+++ msg_exchange/content.js
@@ -2,11 +2,7 @@
  * 
  */
 
-document.addEventListener("DOMContentLoaded", function(){
-	
-	
+$(function() {
+	$("body").append("<div style='width:200px;height:400px;display:block'>hello</div>");
+	$("body div").last().show();
 });
-
-
-
-

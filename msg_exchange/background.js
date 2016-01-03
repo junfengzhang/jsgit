36d28c9c9bd91bbf7fd3239acb9768b1f005eede
junfengzhang <zemmeleta@gmail.com>
2016-01-03 18:13:24 +0800

    11

--- msg_exchange/background.js
+++ msg_exchange/background.js
@@ -1,3 +1,15 @@
-/**
- * 
- */
+//chrome.tabs.onCreated.addListener(function(tab) {
+//	console.log(tab.id);
+//});
+
+
+
+chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
+	console.log("hello");
+	chrome.tabs.getSelected(function(tab) {
+		sendResponse({tabId:tab.id});
+	});
+});
+
+
+
--- msg_exchange/content.js
+++ msg_exchange/content.js
@@ -1,8 +1,47 @@
 /**
  * 
  */
+//.layui-layer-ico{background:url(default/icon.png)
+//var imgURL = chrome.extension.getURL("layer/skin/default/icon.png");
 
-$(function() {
-	$("body").append("<div style='width:200px;height:400px;display:block'>hello</div>");
-	$("body div").last().show();
+var myTabId = chrome
+
+
+//chrome.extension.onRequest.addListener(function(request,sender,sendResp){
+//	
+//	var tabId = request.tabId;
+//	$("#tabId").text(tabId);
+//});
+
+$(function(){	
+	chrome.extension.sendRequest(null,
+			{"a":"b"},function(response){
+		var tabId = response.tabId;
+		$("#tabId").text(tabId);		
+	});	
 });
+
+
+layer.open({
+	type : 1,
+	skin : 'layui-layer-rim', // 加上边框
+	area : [ '420px', '240px' ], // 宽高
+	content : '<div style="position:relative; left:40px; top: 20px;"><span id="tabId"></span><br><textarea type="text" id="session" name= "editer" rows=5 cols = 30/><br>'+
+		'tabId:<input type="text"/><br><input type="button" value="send" id="send" /></div>',
+	shadeClose:true
+});
+
+$("#send").click(function(){
+	alert("hello");
+	var msg = $("#session").val();
+	alert(msg);
+	chrome.tabs.getCurrent(function(tab){		
+		alert(tab.id);
+	});
+});
+
+
+
+
+
+
